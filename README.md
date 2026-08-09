e4d9909661e6cf1fd53d54a2ec1f3797a5d77178
Alex Fargo <me@alexfargo.xyz>
2026-08-08 20:27:43 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Raylib Snake
+
+## Getting Started
+
+Make sure you have `cmake` and `raylib` installed on your computer.
+
+Next, generate the build system using `cmake`
+
+```sh
+# I provide the export compile commands variable for lsp purposes
+$ cmake -S . -B build -DCMAKE_EXPORT_COMPILE_COMMANDS=ON 
+# For lsp...
+$ ln -s build/compile_commands.json .
+```
---
+++ src/main.cpp
@@ -0,0 +1,131 @@
+#include <cstdlib>
+#include <iostream>
+#include <raylib.h>
+#include <raymath.h>
+#include <vector>
+#include <cmath>
+#include <random>
+
+struct Vector2i {
+    int x;
+    int y;
+};
+
+constexpr int TICKS_PER_SECOND = 5;
+constexpr float TIME_PER_TICK = 1.0f / static_cast<float>(TICKS_PER_SECOND);
+
+constexpr int CELL_WIDTH = 32;
+constexpr int CELL_HEIGHT = 32;
+
+/// 16x9 grid + 1 extra cell so there is a center
+constexpr int GRID_WIDTH = 16 * 2 + 1;
+constexpr int GRID_HEIGHT = 9 * 2 + 1;
+constexpr int WINDOW_WIDTH = CELL_WIDTH * GRID_WIDTH;
+constexpr int WINDOW_HEIGHT = CELL_HEIGHT * GRID_HEIGHT;
+
+constexpr Color SNAKE_COLOR = WHITE;
+constexpr Color APPLE_COLOR = GREEN;
+
+constexpr Vector2i GetCenterCellPos() {
+    return{
+        static_cast<int>(GRID_WIDTH / 2),
+        static_cast<int>(GRID_HEIGHT / 2),
+    };
+}
+
+Vector2 CellToScreenCoords(const Vector2i &cell) {
+    return {static_cast<float>(cell.x * CELL_WIDTH), static_cast<float>(cell.y * CELL_HEIGHT)};
+}
+
+inline bool IsVector2Zero(const Vector2 &v) {
+    return Vector2Equals(v, Vector2Zero());
+}
+
+inline bool IsVector2iZero(const Vector2i &v) {
+    return v.x == 0 && v.y == 0;
+}
+
+Vector2i CellOffset(const Vector2i &pos, const Vector2i &offset) {
+    return {pos.x + offset.x, pos.y + offset.y};
+}
+
+float tickAccumulator = 0.0f;
+Vector2i inputDir{};
+Vector2i moveDir{};
+std::vector<Vector2i> snakePoints{};
+std::vector<Vector2i> apples{};
+
+std::mt19937 rng(std::random_device{}());
+
+void UpdateInputDir() {
+    Vector2i newInputVector{};
+    newInputVector.y = IsKeyPressed(KEY_DOWN) - IsKeyPressed(KEY_UP);
+    newInputVector.x = IsKeyPressed(KEY_RIGHT) - IsKeyPressed(KEY_LEFT);
+    if (IsVector2iZero(newInputVector)) return;
+    bool isDiagonalInput = std::abs(newInputVector.x) && std::abs(newInputVector.y);
+    if (isDiagonalInput) return;
+    bool isOppositeDir = (newInputVector.x + moveDir.x) == 0 && (newInputVector.y + moveDir.y) == 0;
+    if (isOppositeDir) return;
+    inputDir = newInputVector;
+}
+
+void DrawSnake() {
+    for (const auto &point : snakePoints) {
+        Vector2 screenCoords = CellToScreenCoords(point);
+        DrawRectangle(screenCoords.x, screenCoords.y, CELL_WIDTH, CELL_HEIGHT, SNAKE_COLOR);
+    }
+}
+
+void DrawApples() {
+    for (const auto &apple : apples) {
+        Vector2 screenCoords = CellToScreenCoords(apple);
+        DrawRectangle(screenCoords.x, screenCoords.y, CELL_WIDTH, CELL_HEIGHT, APPLE_COLOR);
+    }
+}
+
+void UpdateSnakePosition() {
+    if (IsVector2iZero(inputDir)) return;
+    if (snakePoints.empty()) return;
+    Vector2i next = CellOffset(snakePoints.front(), inputDir);
+    snakePoints.pop_back();
+    snakePoints.insert(snakePoints.begin(), next);
+    moveDir = inputDir;
+}
+
+void Update(float delta) {
+    UpdateInputDir();
+}
+
+void TickUpdate(float delta) {
+    UpdateSnakePosition();
+    std::uniform_int_distribution<int> appleSpawnChance(1, 100);
+    int randomNum = appleSpawnChance(rng);
+    if (randomNum < 5) {
+        std::cout << "Spawning Apple!\n";
+        std::uniform_int_distribution<int> appleSpawnLocationX(1, GRID_WIDTH-1);
+        std::uniform_int_distribution<int> appleSpawnLocationY(1, GRID_HEIGHT-1);
+        apples.push_back({appleSpawnLocationX(rng), appleSpawnLocationY(rng)});
+    }
+}
+
+int main(void) {
+    InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Snake!");
+    SetTargetFPS(60);
+    snakePoints.push_back(GetCenterCellPos());
+    while (!WindowShouldClose()) {
+        float frameTime = GetFrameTime();
+        tickAccumulator += frameTime;
+        Update(frameTime);
+        if (tickAccumulator >= TIME_PER_TICK) {
+            TickUpdate(TIME_PER_TICK);
+            tickAccumulator -= TIME_PER_TICK;
+        }
+        BeginDrawing();
+        ClearBackground(DARKGRAY);
+        DrawApples();
+        DrawSnake();
+        EndDrawing();
+    }
+    CloseWindow();
+    return 0;
+}
