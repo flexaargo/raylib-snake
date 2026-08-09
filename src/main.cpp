4cbc8785b0db1afd31721aae7afb484307c6ba91
Alex Fargo <me@alexfargo.xyz>
2026-08-09 14:51:10 -0700

    made apple spawning time based

--- src/main.cpp
+++ src/main.cpp
@@ -31,6 +31,7 @@ constexpr Vector2i CELL_SIZE{CELL_WIDTH, CELL_HEIGHT};
 constexpr int APPLE_WIDTH = 16;
 constexpr int APPLE_HEIGHT = 16;
 constexpr Vector2i APPLE_SIZE{APPLE_WIDTH, APPLE_HEIGHT};
+constexpr float APPLE_SPAWN_TIMEOUT_SECS = 5.0f;
 
 /// 16x9 grid + 1 extra cell so there is a center
 constexpr int GRID_WIDTH = 16 * 2 + 1;
@@ -62,16 +63,17 @@ constexpr Vector2i CellOffset(const Vector2i &pos, const Vector2i &offset) {
 }
 
 float tickAccumulator = 0.0f;
+float appleSpawnAccumulator = APPLE_SPAWN_TIMEOUT_SECS;
 Vector2i inputDir{};
 Vector2i moveDir{};
-std::vector<Vector2i> snakePoints{};
+std::vector<Vector2i> snakeBody{GetCenterCellPos()};
 std::vector<Vector2i> apples{};
 
 std::mt19937 rng(std::random_device{}());
 
 void IncreaseSnakeBody() {
-    if (snakePoints.empty()) return;
-    snakePoints.insert(snakePoints.end(), snakePoints.back());
+    if (snakeBody.empty()) return;
+    snakeBody.insert(snakeBody.end(), snakeBody.back());
 }
 
 void UpdateInputDir() {
@@ -87,7 +89,7 @@ void UpdateInputDir() {
 }
 
 void DrawSnake() {
-    for (const auto &point : snakePoints) {
+    for (const auto &point : snakeBody) {
         Vector2 screenCoords = CellToScreenCoords(point);
         DrawRectangle(screenCoords.x, screenCoords.y, CELL_WIDTH, CELL_HEIGHT, SNAKE_COLOR);
     }
@@ -107,12 +109,12 @@ void DrawApples() {
 
 void UpdateSnakePosition() {
     if (IsVector2iZero(inputDir)) return;
-    if (snakePoints.empty()) return;
+    if (snakeBody.empty()) return;
     // basically looping thru snake in reverse and setting pos = next body pos
-    for (auto it = snakePoints.rbegin(); it != snakePoints.rend(); ++it) {
+    for (auto it = snakeBody.rbegin(); it != snakeBody.rend(); ++it) {
         auto &curr = *it;
         auto nextIt = it + 1;
-        if (nextIt != snakePoints.rend()) {
+        if (nextIt != snakeBody.rend()) {
             curr = *nextIt;
         } else {
             // this is the head of the snake
@@ -126,26 +128,35 @@ void Update(float delta) {
     UpdateInputDir();
 }
 
-void TrySpawnApple() {
-    std::uniform_int_distribution<int> appleSpawnChance(1, 100);
-    int randomNum = appleSpawnChance(rng);
-    if (randomNum < 5) {
-        std::uniform_int_distribution<int> appleSpawnLocationX(1, GRID_WIDTH-1);
-        std::uniform_int_distribution<int> appleSpawnLocationY(1, GRID_HEIGHT-1);
-        Vector2i spawnPosition{};
-        const auto snakeContainsPosition = [spawnPosition](const Vector2i &pos){
-            return Vector2iEquals(pos, spawnPosition);
-        };
-        do {
-            spawnPosition = {appleSpawnLocationX(rng), appleSpawnLocationY(rng)};
-        } while (std::find_if(snakePoints.begin(), snakePoints.end(), snakeContainsPosition) != snakePoints.end());
-        apples.push_back(spawnPosition);
-    }
+bool IsInvalidAppleSpawnPosition(Vector2i spawnPos) {
+    const auto snakeOverlapPos = std::find_if(snakeBody.begin(), snakeBody.end(), [spawnPos](const auto &snakeBodyPos) {
+        return Vector2iEquals(spawnPos, snakeBodyPos);
+    });
+    if (snakeOverlapPos != snakeBody.end()) return true;
+    const auto appleOverlapPos = std::find_if(apples.begin(), apples.end(), [spawnPos](const auto &applePos) {
+        return Vector2iEquals(spawnPos, applePos);
+    });
+    if (appleOverlapPos != apples.end()) return true;
+    return false;
+}
+
+void TrySpawnApple(float delta) {
+    appleSpawnAccumulator += delta;
+    if (appleSpawnAccumulator < APPLE_SPAWN_TIMEOUT_SECS) return;
+    appleSpawnAccumulator = 0.0f;
+
+    std::uniform_int_distribution<int> appleSpawnLocationX(1, GRID_WIDTH-1);
+    std::uniform_int_distribution<int> appleSpawnLocationY(1, GRID_HEIGHT-1);
+    Vector2i spawnPosition{};
+    do {
+        spawnPosition = {appleSpawnLocationX(rng), appleSpawnLocationY(rng)};
+    } while (IsInvalidAppleSpawnPosition(spawnPosition));
+    apples.push_back(spawnPosition);
 }
 
 void CheckAppleCollision() {
-    if (snakePoints.empty()) return;
-    Vector2i &head = snakePoints.front();
+    if (snakeBody.empty()) return;
+    Vector2i &head = snakeBody.front();
     int i = 0;
     for (const auto &apple : apples) {
         if (Vector2iEquals(apple, head)) {
@@ -160,20 +171,19 @@ void CheckAppleCollision() {
 void TickUpdate(float delta) {
     UpdateSnakePosition();
     CheckAppleCollision();
-    TrySpawnApple();
+    TrySpawnApple(delta);
 }
 
 int main(void) {
     InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Snake!");
     SetTargetFPS(60);
-    snakePoints.push_back(GetCenterCellPos());
     while (!WindowShouldClose()) {
         float frameTime = GetFrameTime();
         tickAccumulator += frameTime;
         Update(frameTime);
         if (tickAccumulator >= TIME_PER_TICK) {
-            TickUpdate(TIME_PER_TICK);
-            tickAccumulator -= TIME_PER_TICK;
+            TickUpdate(tickAccumulator);
+            tickAccumulator = 0;
         }
         BeginDrawing();
         ClearBackground(DARKGRAY);
